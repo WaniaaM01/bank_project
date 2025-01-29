eb7b24542b9f2e53072cf212d86eb6eabaedfa6e
WaniaaM01 <waniaamasood5@gmail.com>
2025-01-29 22:39:34 +0500

    Update account_type2.cpp

--- account_type2.cpp
+++ account_type2.cpp
@@ -1,44 +1,36 @@
-#include <iostream>
-#include <string>
-using namespace std;
-
-class AccountType{
+class AccountType {
     public:
-    string name;
-    double dailywithdrawalLimit;
-    
-    AccountType(){};
-    ~AccountType(){};
+    string name;                // Stores account type name
+    double dailywithdrawalLimit; // Maximum amount you can withdraw per day
+
+    // Checks if a withdrawal is allowed
     bool canwithdraw(double amount){
+        // Can't withdraw negative or zero amount
         if(amount <= 0) return false;
+        // Can't withdraw more than daily limit
         if(amount > dailywithdrawalLimit) return false;
         return true;
     }
-    double getDailyWithdrawalLimit(){
-        return dailywithdrawalLimit;
-    }
-};
+}
 
-class Bronze : public AccountType{
-    public:
+// Different account types with their limits
+class Bronze : public AccountType {
     Bronze(){
         name = "Bronze";
-        dailywithdrawalLimit = 100000.0;
+        dailywithdrawalLimit = 100000.0;  // Bronze accounts can withdraw up to 100,000 per day
     }
-};
+}
 
-class Gold : public AccountType{
-    public:
+class Gold : public AccountType {
     Gold(){
         name = "Gold";
-        dailywithdrawalLimit = 500000.0;
+        dailywithdrawalLimit = 500000.0;  // Gold accounts can withdraw up to 500,000 per day
     }
-};
+}
 
-class Business : public AccountType{
-    public:
+class Business : public AccountType {
     Business(){
         name = "Business";
-        dailywithdrawalLimit = 20000000.0;
+        dailywithdrawalLimit = 20000000.0;  // Business accounts can withdraw up to 20,000,000 per day
     }
-};
+}
